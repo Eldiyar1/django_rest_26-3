57ab39353b9f13f4be7543419084d267dbdf061e
Eldiyar1 <mc.oks1ne@gmail.com>
2023-07-03 20:23:18 +0600

    done

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import test_api_view, ads_list_api_view, ads_detail_api_view
 from .views import CategoryListAPIView, CategoryDetailAPIView, \
     HashTagAPIView, AdsListAPIView
 
 urlpatterns = [
-    path('test/', test_api_view),
-    path('ads/<int:id>/', ads_detail_api_view),
     path('ads/', AdsListAPIView.as_view()),
     path('categories/', CategoryListAPIView.as_view()),
     path('categories/<int:id>/', CategoryDetailAPIView.as_view()),
